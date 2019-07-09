12fd6eb90821b374f59a8475d07869488c373833
smart <smart@onTop.com>
2019-07-09 11:30:14 +0800

    add RedisUtil function

---
+++ src/main/java/com/jxlg/logistic/utils/RedisUtil.java
@@ -0,0 +1,29 @@
+package com.jxlg.logistic.utils;
+
+import javax.annotation.Resource;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+public class RedisUtil {
+	@Resource
+	private static RedisTemplate<String, Object> redisTemplate;
+
+	public static void set(String key, Object value) {
+		RedisSerializer serializer = new StringRedisSerializer();
+		// 设置key编码方式
+		redisTemplate.setKeySerializer(serializer);
+
+		ValueOperations<String, Object> opsForValue = redisTemplate.opsForValue();
+		opsForValue.set(key, value);
+	}
+
+	public static Object get(String key) {
+		ValueOperations<String, Object> opsForValue = redisTemplate.opsForValue();
+		Object value = opsForValue.get(key);
+		return value;
+	}
+
+}
